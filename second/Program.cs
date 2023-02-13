f2e2f123840f98d62c09a634680b6860203ddd82
DianaKoshlakova <di.koshlakova.nkv@yandex.ru>
2023-02-13 16:09:11 +0300

    second

---
+++ second/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ") ;
+int x = int.Parse(Console.ReadLine()) ;
+
+if( (x % 2) == 0 )
+{
+  Console.WriteLine(" Чётное ") ;
+}
+else
+{
+  Console.WriteLine(" Нечётное ");
+}
