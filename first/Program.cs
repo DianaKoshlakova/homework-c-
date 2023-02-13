d1f00d04afba52ed202b3823974bd72f5175d85c
DianaKoshlakova <di.koshlakova.nkv@yandex.ru>
2023-02-13 15:05:50 +0300

    первый

---
+++ first/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = new Random().Next(1,11);
+Console.WriteLine(a);
+int b = new Random().Next(3,12);
+Console.WriteLine(b);
+int c = new Random().Next(13,16);
+Console.WriteLine(c);
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write(" max= ");
+Console.WriteLine(max);
